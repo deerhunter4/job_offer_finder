693f13abd4f0a87c4436d201216bee06faca55be
Mateusz <mateusz.j.buczek@gmail.com>
2024-03-06 11:45:47 +0100

    Updating variable names in API_requests_test.py file
    I updated varable names (e.g. shortcuts like 'res') according
    to the clean code rules.

--- API_request_test.py
+++ API_request_test.py
@@ -12,44 +12,44 @@ import json
 # https://www.weatherapi.com/signup.aspx
 # The url is set to get forecast data. For historical data it should be changed
 
-url = 'https://api.weatherapi.com/v1/forecast.json'
-API_KEY = <paste here your personal API key>
+url_wheatherapi = 'https://api.weatherapi.com/v1/forecast.json'
+API_KEY_wheatherapi = <paste here your personal API key>
 
 # You can change the name of the city <q parameter>, search engine is quite efficient
 # days=1 means that you get hourly forecast for one date, the day you are make a request. 
 # To get forecast for next day you need additional parameter
-params = dict(key=API_KEY, q='Przemysl', days=1) 
+params_wheatherapi = dict(key= API_KEY_wheatherapi, q='Przemysl', days=1) 
 
-res = requests.get(url, params=params)
+resonse_wheatherapi = requests.get(url_wheatherapi, params= params_wheatherapi)
 
-# print(res)
-# print(res.status_code)
-# print(res.headers)
-# print(res.json())
+# print(resonse_wheatherapi)
+# print(resonse_wheatherapi.status_code)
+# print(resonse_wheatherapi.headers)
+# print(resonse_wheatherapi.json())
 
-# text = json.dumps(res.json(), sort_keys=True, indent=4)
+# text = json.dumps(resonse_wheatherapi.json(), sort_keys=True, indent=4)
 # print(text)
 
-res_dict = res.json()
-# res_dict.keys()
-# res_forecast = res_dict['forecast']
-# res_forecast.keys()
-# res_forecastday = res_forecast['forecastday']
+resonse_wheatherapi_dict = resonse_wheatherapi.json()
+# resonse_wheatherapi_dict.keys()
+# rresonse_wheatherapi_forecast = resonse_wheatherap_dict['forecast']
+# resonse_wheatherapi_forecast.keys()
+# resonse_wheatherapi_forecastday = resonse_wheatherap_forecast['forecastday']
 
-res_hours = res_dict['forecast']['forecastday'][0]['hour']
+resonse_wheatherapi_hours = resonse_wheatherapi_dict['forecast']['forecastday'][0]['hour']
 # res_hours[4]
 
-hours = []
-temp_c = []
-rain = []
-for item in res_hours:
-    hours.append(item["time"])
-    temp_c.append(item["temp_c"])
-    rain.append(item['precip_mm'])
+hours_wheatherapi = []
+temp_c_wheatherapi = []
+rain_wheatherapi = []
+for item in resonse_wheatherapi_hours:
+    hours_wheatherapi.append(item["time"])
+    temp_c_wheatherapi.append(item["temp_c"])
+    rain_wheatherapi.append(item['precip_mm'])
 
-print(hours)
-print(temp_c)
-print(rain)
+print(hours_wheatherapi)
+print(temp_c_wheatherapi)
+print(rain_wheatherapi)
 
 ######################
 ### open-meteo.com ###
@@ -59,35 +59,35 @@ print(rain)
 # to request you need to give latitute and longitute of the location,
 # however we can get this data from request above, because we get this info as an output
 
-lat = res_dict['location']['lat']
-lon = res_dict['location']['lon']
+latitude = resonse_wheatherapi_dict['location']['lat']
+longitude = resonse_wheatherapi_dict['location']['lon']
 
-url_2 = "https://api.open-meteo.com/v1/forecast"
-params_2 = {
-	"latitude": lat,
-	"longitude": lon,
+url_openmeteo = "https://api.open-meteo.com/v1/forecast"
+params_openmeteo = {
+	"latitude": latitude,
+	"longitude": longitude,
 	"hourly": ["temperature_2m", "precipitation"], # here we can extend number of parameters
 	"forecast_days": 1
 }
 
-res_2 = requests.get(url_2, params=params_2)
-res_2_dict = res_2.json()
+response_openmeteo = requests.get(url_openmeteo, params=params_openmeteo)
+response_openmeteo_dict = response_openmeteo.json()
 
-hours_2 = res_2_dict['hourly']['time']
-temp_c_2 = res_2_dict['hourly']['temperature_2m']
-rain_2 = res_2_dict['hourly']['precipitation']
+hours_openmeteo = response_openmeteo_dict['hourly']['time']
+temp_c_openmeteo = response_openmeteo_dict['hourly']['temperature_2m']
+rain_openmeteo = response_openmeteo_dict['hourly']['precipitation']
 
 # below you can compare forecasts from bothe webpages, and yes, they differ :)
 
-print(hours_2)
-print(temp_c_2)
-print(rain_2)
+print(hours_openmeteo)
+print(temp_c_openmeteo)
+print(rain_openmeteo)
 
-print(hours)
-print(hours_2)
+print(hours_wheatherapi)
+print(hours_openmeteo)
 
-print(temp_c)
-print(temp_c_2)
+print(temp_c_wheatherapi)
+print(temp_c_openmeteo)
 
 
 #####################
@@ -98,38 +98,38 @@ print(temp_c_2)
 # https://rapidapi.com/meteostat/api/meteostat/pricing
 # The url is set to get forecast data. For historical data it should be changed
 
-url_3 = 'https://meteostat.p.rapidapi.com/point/hourly'
-API_KEY_3 = <paste here your personal API key>
+url_meteostat = 'https://meteostat.p.rapidapi.com/point/hourly'
+API_KEY_meteostat = <paste here your personal API key>
 
-params_3 = {
-	"lat": lat,
-	"lon": lon,
-	"start":"2024-02-28", #here we have to use some timestump to do it automaticaly
-    "end":"2024-02-28"
+params_meteostat = {
+	"lat": latitude,
+	"lon": longitude,
+	"start":"2024-03-06", #here we have to use some timestump to do it automaticaly
+    "end":"2024-03-06"
 }
 
-headers_3 = {
+headers_meteostat = {
 	"X-RapidAPI-Key": <paste here your personal API key>,
 	"X-RapidAPI-Host": "meteostat.p.rapidapi.com"
 }
 
-res_3 = requests.get(url_3, headers= headers_3, params= params_3)
-res_3_dict = res_3.json()
+response_meteostat = requests.get(url_meteostat, headers= headers_meteostat, params= params_meteostat)
+response_meteostat_dict = response_meteostat.json()
 
-hours_3 = []
-temp_c_3 = []
-rain_3 = []
-for item in res_3_dict["data"]:
-    hours_3.append(item['time'])
-    temp_c_3.append(item['temp'])
-    rain_3.append(item['prcp'])
+hours_meteostat = []
+temp_c_meteostat = []
+rain_meteostat = []
+for item in response_meteostat_dict["data"]:
+    hours_meteostat.append(item['time'])
+    temp_c_meteostat.append(item['temp'])
+    rain_meteostat.append(item['prcp'])
     
-print(hours_3)
-print(temp_c_3)
-print(rain_3)
+print(hours_meteostat)
+print(temp_c_meteostat)
+print(rain_meteostat)
 
-print(temp_c), print(temp_c_2), print(temp_c_3)
+print(temp_c_wheatherapi), print(temp_c_openmeteo), print(temp_c_meteostat)
 
-print(rain), print(rain_2), print(rain_3)
+print(rain_wheatherapi), print(rain_openmeteo), print(rain_meteostat)
 
-print(hours[1]), print(hours_2[1]), print(hours_3[1]) # three different time formats
+print(hours_wheatherapi[1]), print(hours_openmeteo[1]), print(hours_meteostat[1]) # three different time formats
