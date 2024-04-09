945b8daa58da556d30bc7103ae6f8d34891bf12e
Mateusz <mateusz.j.buczek@gmail.com>
2024-04-09 12:21:13 +0200

    [Args][P1] Implement Argument Parser Component
    I created arguments.py file. This component
    is responsible for handling user input (parameters).

---
+++ arguments.py
@@ -0,0 +1,29 @@
+import sys
+import argparse
+
+
+def parameters():
+    parser = argparse.ArgumentParser(
+            prog='BetterWeather',
+            description="""This program is written in Python.
+            Its purpose is to collect weather forecast data
+            from a few free weather forecast APIs (e.g. Open-Meteo, WeatherAPI)
+            and combine it into one more accurate weather prediction.
+            After providing the location eg. "Madrid" you will receive a file
+            with weather data and a user-friendly plot.""")
+
+    # positional arguments
+    parser.add_argument('location', metavar='location', type=str,
+                        help="""Enter the location for which you want
+                         to get the weather forecast e.g. "Cracovia".""")
+
+    # optional arguments
+    parser.add_argument('-fp', metavar='forecast_period', type=str,
+                        choices=['24h', '48h', '72h', '7days', '10days', '14days'],
+                        help="""Enter the forecast period you are interested
+                         in. Valid options are: 24h, 48h, 72h, 7days, 10days,
+                         14days (default: 24h).""", default='24h')
+
+    # if no arguments were given, printing the help message (args = "--help")
+    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+    return args
