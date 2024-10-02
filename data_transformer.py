05242d345ff625775922b26c11d7e5d32c2b988d
Mateusz <mateusz.j.buczek@gmail.com>
2024-10-02 19:25:33 +0200

    [Dat][P3] data_transformer.py update Two new functions created: check_date and check_hour. create_dataframe function simplified Variable names added .gitignore file corrected

--- data_transformer.py
+++ data_transformer.py
@@ -1,26 +1,40 @@
 import pandas as pd
 
 
+def check_date(forecast_dataframe):
+    meteostat_date = forecast_dataframe['hours_meteostat'][0][:10]
+    openmeteo_date = forecast_dataframe['hours_openmeteo'][0][:10]
+    weatherapi_date = forecast_dataframe['hours_weatherapi'][0][:10]
+    if meteostat_date == openmeteo_date and meteostat_date == weatherapi_date:
+        forecast_dataframe['date'] = meteostat_date
+
+
+def check_hour(forecast_dataframe):
+    for row_no in range(0, len(forecast_dataframe)):
+        meteostat_hour = forecast_dataframe['hours_meteostat'][row_no][11:16]
+        openmeteo_hour = forecast_dataframe['hours_openmeteo'][row_no][11:16]
+        weatherapi_hour = forecast_dataframe['hours_weatherapi'][row_no][11:16]
+        if meteostat_hour == openmeteo_hour == weatherapi_hour:
+            forecast_dataframe.loc[row_no, 'hour'] = meteostat_hour
+
+
 def create_dataframe(dict1, dict2, dict3):
     all_data_dict = {**dict1, **dict2, **dict3}
-    df = pd.DataFrame(all_data_dict)
+    full_forecast_dataframe = pd.DataFrame(all_data_dict)
+    full_forecast_dataframe.insert(loc=1, column='hour', value="empty")
 
     # check if for each forecast point the time period (date and hour) is the same and create variables date and hour
-    if df['hours_meteostat'][0][:10] == df['hours_openmeteo'][0][:10] and df['hours_meteostat'][0][:10] == df['hours_weatherapi'][0][:10]:
-        df['date'] = df['hours_meteostat'][0][:10]
+    check_date(full_forecast_dataframe)
+    check_hour(full_forecast_dataframe)
 
-    df.insert(loc=1, column='hour', value="empty")
-    for row_no in range(0, len(df)):
-        if df['hours_meteostat'][row_no][11:16] == df['hours_openmeteo'][row_no][11:16] == df['hours_weatherapi'][row_no][11:16]:
-            df.loc[row_no, 'hour'] = df['hours_meteostat'][row_no][11:16]
+    # remove redundand time columns
+    full_forecast_dataframe = full_forecast_dataframe.drop(columns=['hours_meteostat', 'hours_openmeteo', 'hours_weatherapi'])
 
-    df = df.drop(columns=['hours_meteostat', 'hours_openmeteo', 'hours_weatherapi'])
+    # sort full_forecast_dataframe by column names
+    full_forecast_dataframe = full_forecast_dataframe.reindex(sorted(full_forecast_dataframe.columns), axis=1)
 
-    # sort DF by column names
-    df = df.reindex(sorted(df.columns), axis=1)
+    # count mean temperature and mean precipitation per each time point
+    full_forecast_dataframe['mean_rain'] = full_forecast_dataframe.loc[:, ['rain_meteostat', 'rain_openmeteo', 'rain_weatherapi']].mean(axis=1).round(1)
+    full_forecast_dataframe['mean_temp'] = full_forecast_dataframe.loc[:, ['temp_meteostat', 'temp_openmeteo', 'temp_weatherapi']].mean(axis=1).round(1)
 
-    # count mean temperature and mean precipitation
-    df['mean_rain'] = df.loc[:, ['rain_meteostat', 'rain_openmeteo', 'rain_weatherapi']].mean(axis=1).round(1)
-    df['mean_temp'] = df.loc[:, ['temp_meteostat', 'temp_openmeteo', 'temp_weatherapi']].mean(axis=1).round(1)
-
-    return df
+    return full_forecast_dataframe
